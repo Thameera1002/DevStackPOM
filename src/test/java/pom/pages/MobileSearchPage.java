fc0a24ed0c31d0caf974501bb6bcdda22b964eaf
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-02-12 19:54:56 +0530

    MobileSearchPage updated and SearchTest added

--- src/test/java/pom/pages/MobileSearchPage.java
+++ src/test/java/pom/pages/MobileSearchPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import pom.utils.WebActions;
 
 public class MobileSearchPage {
 
@@ -18,5 +20,15 @@ public class MobileSearchPage {
     @FindBy(xpath = "//button[@id='gh-search-btn']")
     private WebElement searchButton;
 
+    public MobileSearchPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void searchMobile(String mobileName){
+        WebActions.type(searchBox, mobileName);
+        WebActions.selectDropdownByText(categoryDropDown,"Cell Phones & Accessories");
+        WebActions.click(searchButton);
+    }
 
 }
