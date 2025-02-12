86d4c36d6cd4004a86e67d8d21d2aaf168545228
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-02-12 19:33:44 +0530

    Update WebActions.java

--- src/test/java/pom/utils/WebActions.java
+++ src/test/java/pom/utils/WebActions.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -12,6 +14,7 @@ import java.time.Duration;
 public class WebActions {
 
     private static final int TIMEOUT_SECONDS = 10;
+    private static final int POLLING = 500;
 
     public static WebDriver getDriver() {
         return TreadLocalWebDriver.getWebDriver();
@@ -25,7 +28,12 @@ public class WebActions {
 
     //fluent wait
     private static WebElement fluentWaitForElement(WebElement element) {
+        FluentWait<WebDriver> wait = new FluentWait<>(getDriver())
+                .withTimeout(Duration.ofSeconds(TIMEOUT_SECONDS))
+                .ignoring(NoSuchElementException.class)
+                .pollingEvery(Duration.ofMillis(POLLING));
 
+        return wait.until(ExpectedConditions.visibilityOf(element));
     }
 
     public static void click(WebElement element) {
@@ -37,5 +45,26 @@ public class WebActions {
         }
     }
 
+
+    public static void type(WebElement element, String text) {
+        try{
+            WebElement el = fluentWaitForElement(element);
+            el.click();
+            el.clear();
+            el.sendKeys(text);
+        }catch (NoSuchElementException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void selectDropdownByText(WebElement element, String text) {
+        try{
+            WebElement el = waitForElement(element);
+            Select select = new Select(el);
+            select.selectByVisibleText(text);
+        }catch (NoSuchElementException e){
+            e.printStackTrace();
+        }
+    }
 
 }
