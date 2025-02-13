e31fc3916da62ba14198584752852ae22dda0f01
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-02-13 19:06:08 +0530

    Update SearchTest.java

--- src/test/java/pom/tests/SearchTest.java
+++ src/test/java/pom/tests/SearchTest.java
@@ -6,29 +6,41 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pom.pages.ClothSearchPage;
 import pom.pages.MobileSearchPage;
+import pom.pages.SearchItemPage;
 import pom.utils.TreadLocalWebDriver;
 
 public class SearchTest {
-    private MobileSearchPage mobileSearchPage;
-    private ClothSearchPage clothSearchPage;
+//    private MobileSearchPage mobileSearchPage;
+//    private ClothSearchPage clothSearchPage;
+    private SearchItemPage searchItemPage;
 
     @BeforeMethod
     public void setup(){
         WebDriver driver = TreadLocalWebDriver.createWebDriver();
         driver.manage().window().maximize();
         driver.get("https://www.ebay.com/");
-        mobileSearchPage = new MobileSearchPage(driver);
-        clothSearchPage = new ClothSearchPage(driver);
+//        mobileSearchPage = new MobileSearchPage(driver);
+//        clothSearchPage = new ClothSearchPage(driver);
+        searchItemPage = new SearchItemPage(driver);
+    }
+
+//    @Test
+//    public void searchForIphone() throws InterruptedException {
+//        mobileSearchPage.searchMobile("iPhone");
+//    }
+//
+//    @Test
+//    public void searchForMensTShirts() throws InterruptedException {
+//        clothSearchPage.searchClothing("Men's T-shirt");
+//    }
+    @Test
+    public void searchItemIphone() throws InterruptedException {
+        searchItemPage.searchItem("iphone","Cell Phones & Accessories");
     }
 
     @Test
-    public void searchForIphone() throws InterruptedException {
-        mobileSearchPage.searchMobile("iPhone");
-    }
-
-    @Test
-    public void searchForMensTShirts() throws InterruptedException {
-        clothSearchPage.searchClothing("Men's T-shirt");
+    public void searchItemClothing() throws InterruptedException {
+        searchItemPage.searchItem("Men's t-shirt","Clothing, Shoes & Accessories");
     }
 
     @AfterMethod
