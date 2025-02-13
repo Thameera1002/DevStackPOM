18518452ee5b50140c734ed22a924aebd640e2f7
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-02-13 19:23:08 +0530

    Update SearchTest.java

--- src/test/java/pom/tests/SearchTest.java
+++ src/test/java/pom/tests/SearchTest.java
@@ -3,6 +3,7 @@ package pom.tests;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pom.pages.ClothSearchPage;
 import pom.pages.MobileSearchPage;
@@ -13,6 +14,15 @@ public class SearchTest {
 //    private MobileSearchPage mobileSearchPage;
 //    private ClothSearchPage clothSearchPage;
     private SearchItemPage searchItemPage;
+
+
+    @DataProvider(name = "searchData")
+    public Object[][] searchItemData(){
+        return new Object[][]{
+                {"iphone","Cell Phones & Accessories"},
+                {"Men's T-shirts","Clothing, Shoes & Accessories"}
+        };
+    }
 
     @BeforeMethod
     public void setup(){
@@ -33,14 +43,19 @@ public class SearchTest {
 //    public void searchForMensTShirts() throws InterruptedException {
 //        clothSearchPage.searchClothing("Men's T-shirt");
 //    }
-    @Test
-    public void searchItemIphone() throws InterruptedException {
-        searchItemPage.searchItem("iphone","Cell Phones & Accessories");
-    }
+//    @Test
+//    public void searchItemIphone() throws InterruptedException {
+//        searchItemPage.searchItem("iphone","Cell Phones & Accessories");
+//    }
+//
+//    @Test
+//    public void searchItemClothing() throws InterruptedException {
+//        searchItemPage.searchItem("Men's t-shirt","Clothing, Shoes & Accessories");
+//    }
 
-    @Test
-    public void searchItemClothing() throws InterruptedException {
-        searchItemPage.searchItem("Men's t-shirt","Clothing, Shoes & Accessories");
+    @Test(dataProvider = "searchData")
+    public void searchForItem(String itemName,String categoryName) throws InterruptedException {
+        searchItemPage.searchItem(itemName,categoryName);
     }
 
     @AfterMethod
