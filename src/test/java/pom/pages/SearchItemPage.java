0a2d67d8f972e3d8d4f36f132c03f0153f4f1a2d
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-02-13 18:58:06 +0530

    Create SearchItemPage.java

---
+++ src/test/java/pom/pages/SearchItemPage.java
@@ -0,0 +1,34 @@
+package pom.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import pom.utils.WebActions;
+
+public class SearchItemPage {
+
+    private WebDriver driver;
+
+    @FindBy(xpath = "//input[@id='gh-ac']")
+    private WebElement searchBox;
+
+    @FindBy(xpath = "//select[@id='gh-cat']")
+    private WebElement categoryDropDown;
+
+    @FindBy(xpath = "//button[@id='gh-search-btn']")
+    private WebElement searchButton;
+
+    public SearchItemPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void searchItem(String itemName,String categoryName) throws InterruptedException {
+        WebActions.type(searchBox, itemName);
+        Thread.sleep(1000);
+        WebActions.selectDropdownByText(categoryDropDown,categoryName);
+        Thread.sleep(1000);
+        WebActions.click(searchButton);
+    }
+}
